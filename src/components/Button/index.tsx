092cfda2cc4b8903a76447d07bb142e5f861c264
Danny Fung <dannyfung97@gmail.com>
2021-02-07 00:15:31 -0800

    cells logic

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { CellState, CellValue } from '../../types';
 import './Button.scss';
 
-const Button: React.FC = () => {
-    return <div className='Button' />;
+interface ButtonProps {
+    row: number;
+    col: number;
+    state: CellState;
+    value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = ({row, col, state, value}) => {
+    const renderContent = (): React.ReactNode => {
+        if(state == CellState.dug) {
+            if(value == CellValue.mine) {
+                return <span role='img' aria-label='mine'>💣</span>
+            }
+            return value == CellValue.none ? null: value;
+        } 
+        else if (state == CellState.flagged) {
+            return <span role='img' aria-label='flag'>🚩</span>
+        }
+        return null;
+    }
+
+    return <div className={`Button ${state == CellState.dug ? 'dug' : ''} value-${value}`} >
+        {renderContent()}
+    </div>;
 }
 
 export default Button;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
-import { MAX_ROWS, MAX_COLS } from '../constants';
+import { MAX_ROWS, MAX_COLS, MAX_MINES } from '../constants';
 import { Cell, CellValue, CellState } from '../types';
 
 export const generateCells = (): Cell[][] => {
-    const cells: Cell[][] = [];
+    let cells: Cell[][] = [];
 
     for(let row = 0; row < MAX_ROWS; row++) {
         cells.push([]);
@@ -14,5 +14,73 @@ export const generateCells = (): Cell[][] => {
         }
     }
 
+    let minesPlaced = 0;
+
+    while(minesPlaced < MAX_MINES) {
+        const randomRow = Math.floor(Math.random() * MAX_ROWS);
+        const randomCol = Math.floor(Math.random() * MAX_COLS);
+
+        const currentCell = cells[randomRow][randomCol];
+        if(currentCell.value != CellValue.mine){
+            // cells = cells.map((row, rowIndex) => row.map((cell, colIndex) => {
+            //     if(randomRow == rowIndex && randomCol == colIndex) {
+            //         return {
+            //             ...cell,
+            //             value: CellValue.mine
+            //         }
+            //     }
+            //     return cell;
+            // }))
+            cells[randomRow][randomCol].value = CellValue.mine;
+            minesPlaced++;
+        }
+    }
+
+    for(let rowIndex = 0; rowIndex < MAX_ROWS; rowIndex++){
+        for(let colIndex = 0; colIndex < MAX_COLS; colIndex++){
+            let currentCell = cells[rowIndex][colIndex];
+            if(currentCell.value != CellValue.mine){
+                let numMines = 0;
+                const topLeftMine = rowIndex > 0 && colIndex > 0 ? cells[rowIndex - 1][colIndex - 1] : null;
+                const topMine = rowIndex > 0 ? cells[rowIndex - 1][colIndex] : null;
+                const topRightMine = rowIndex > 0 && colIndex < MAX_COLS - 1 ? cells[rowIndex - 1][colIndex + 1] : null;
+
+                const leftMine = colIndex > 0 ? cells[rowIndex][colIndex - 1] : null;
+                const rightMine = colIndex < MAX_COLS - 1 ? cells[rowIndex][colIndex + 1] : null;
+
+                const bottomLeftMine = rowIndex < MAX_ROWS - 1 && colIndex > 0 ? cells[rowIndex + 1][colIndex - 1] : null;
+                const bottomMine = rowIndex < MAX_ROWS - 1 ? cells[rowIndex + 1][colIndex] : null;
+                const bottomRightMine = rowIndex < MAX_ROWS - 1 && colIndex < MAX_COLS - 1 ? cells[rowIndex + 1][colIndex + 1] : null;
+            
+                if(topLeftMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(topMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(topRightMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(leftMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(rightMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(bottomLeftMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(bottomMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(bottomRightMine?.value == CellValue.mine){
+                    numMines++;
+                }
+                if(numMines > 0){
+                    cells[rowIndex][colIndex].value = numMines
+                }
+            }
+        }
+    }
     return cells;
 }
