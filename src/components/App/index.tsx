707d157d158c4ee05e6c44128dbaff2a1cfe3d3f
Danny Fung <dannyfung97@gmail.com>
2021-02-06 19:59:51 -0800

    styling ui is complete

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NumberDisplay from '../NumberDisplay';
+import Button from '../Button';
+import { generateCells } from '../../utils';
 import './App.scss';
 
 const App: React.FC = () => {
+    const [cells, setCells] = useState(generateCells());
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) =>
+            row.map((cell, colIndex) =>
+                <Button key={`${rowIndex}-${colIndex}`} />
+            )
+        );
+    };
+
     return (
         <div className="App">
             <div className='Header'>
@@ -12,7 +24,7 @@ const App: React.FC = () => {
                 <NumberDisplay value={10} />
             </div>
             <div className='Body'>
-                Body
+                {renderCells()}
             </div>
         </div>
     )
